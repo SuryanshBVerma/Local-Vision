deb2e0279f3bbb39a5745b7eed6338033ffd847f
Suryansh Bachchan Verma <76619671+SuryanshBVerma@users.noreply.github.com>
2025-10-28 11:40:54 +0530

    Configured project orchestration

--- Loacl-Vision-AppHost/AppHost.cs
+++ Loacl-Vision-AppHost/AppHost.cs
@@ -1,3 +1,18 @@
+using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var minio = builder.AddContainer("minio", "quay.io/minio/minio")
+    .WithArgs("server", "/data", "--console-address", ":9090")
+    .WithEnvironment("MINIO_ROOT_USER", "admin")
+    .WithEnvironment("MINIO_ROOT_PASSWORD", "password123")
+    //.WithVolume("./minio/data", "/data")
+    .WithEndpoint(9090, targetPort: 9090);
+
+
+var imageService = builder.AddProject<Projects.Backend>("imageservice");
+
+var gateway = builder.AddProject<Projects.Gateway>("gateway")
+    .WithReference(imageService);
 
 builder.Build().Run();
